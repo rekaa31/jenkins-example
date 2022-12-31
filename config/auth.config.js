204e4fdb021543948b9c1abf2c1b138a7a1cda14
adevaraz <zaraveda.zv@gmail.com>
2022-12-31 17:27:34 +0700

    feat(api): create login api

---
+++ config/auth.config.js
@@ -0,0 +1,5 @@
+const config = {
+  secret: process.env.SECRET_KEY,
+};
+
+module.exports = config;
---
+++ controllers/v1/Auth/auth.controller.js
@@ -0,0 +1,50 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const db = require("../../../models");
+const Users = db.users;
+const config = require("../../../config/auth.config.js");
+
+exports.login = async (req, res) => {
+  try {
+    // Validate request
+		if (!req.body) {
+			res.status(400).send({ message: "Content can not be empty!" });
+			return;
+		}
+
+    const user = await Users.find({
+      nik: req.body.nik
+    });
+
+    if(user.length === 0) {
+      res.status(403).send({ message: "NIK or password is not correct" });
+			return;
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(req.body.password, user[0].password);
+
+    if(!isPasswordCorrect) {
+      res.status(403).send({ message: "NIK or password is not correct" });
+			return;
+    }
+
+    const token = jwt.sign(
+      {
+        user_id: user[0]._id,
+      },
+      config.secret,
+      { expiresIn: 3600 } // expires in an hour
+    )
+
+    res.status(200).send({
+			message: "Login Success!",
+			token
+		});
+  } catch(error) {
+    console.log(error)
+    res.status(500).send({
+      message:
+      error.message || "Some error occurred while creating the Permissions."
+    });
+  }
+}
